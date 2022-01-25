42414f1128d66f08c84805cef6265e846807789b
Jelena A <94282497+jelenaart@users.noreply.github.com>
2022-01-25 21:31:20 +0200

    Update slovar.py

--- slovar/slovar.py
+++ slovar/slovar.py
@@ -4,20 +4,6 @@ eesti=[]
 english=failist_lugemine('english.txt',english)
 eesti=failist_lugemine('eesti.txt',eesti)
 
-#valik=""
-#while True:
-#    try:
-#        valik=int(input("Valige: (1)Tõlkida english-estonian\n(2)Tõlkida eesti-inglise\n(3)test yourselfNOT DONE\n(4)Leave"))
-#        if valik==1:
-#            engest()
-#        elif valik==2:
-#            esteng()
-     
-#        else:
-#            print("Something went wrong. Please try again!")
-#    except ValueError:
-#        print("Try again please!")
-
 while True:
     try:
         menu=input('Koik sonad - S, Tolgida-T, Uus sona-U, viga-V, kontroll-K,lopp-L')
@@ -29,11 +15,14 @@ while True:
             print(english)
             print(eesti)
         elif menu.upper()=="T":
-            translate()
+            translate(eesti,english)
         elif menu.upper()=="L":
+            quit
             break
         elif menu.upper()=="V":
-            viga()
+            viga(tolk,l)
+        elif menu.upper()=="K":
+            kontrol(eesti,english)
         else:
             print("Proovi veel kord")
     except ValueError:
